18f10c5163917e6d6565c78745bae9e1c15cd8e5
AsthaKirar <asthap602@gmail.com>
2024-11-22 15:15:18 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,78 @@
-import React from 'react'
-import {motion} from 'framer-motion'
-const App = () => {
-  return (
-    <motion.div 
-    initial={{
-      top:0,
-      left:0
-    }}
-    animate={{
-      x:500,
-      y:500,
-      rotate:360
-    }}
-    whileHover={{
-      backgroundColor:'blue'
-    }}
-    whileTap={{
-      scale:0.95
-    }}
-    whileDrag={{
-      borderRadius:'50%'
-    }}
-    dragConstraints={{
-      left:0,
-      top:0
+// import React from 'react'
+// import {motion} from 'framer-motion'
+// const App = () => {
+//   return (
+//     <motion.div 
+//     initial={{
+//       top:0,
+//       left:0
+//     }}
+//     animate={{
+//       x:500,
+//       y:500,
+//       rotate:360
+//     }}
+//     whileHover={{
+//       backgroundColor:'blue'
+//     }}
+//     whileTap={{
+//       scale:0.95
+//     }}
+//     whileDrag={{
+//       borderRadius:'50%'
+//     }}
+//     dragConstraints={{
+//       left:0,
+//       top:0
 
-    }}
-    dragDirectionLock='true'
+//     }}
+//     dragDirectionLock='true'
 
-     id='box'>
+//      id='box'>
       
 
-    </motion.div>
+//     </motion.div>
+//   )
+// }
+
+// export default App
+// import { motion } from 'framer-motion'
+// import React from 'react'
+
+// const App = () => {
+//   const move = {
+//     start:{
+//       x:0,
+//       rotate:0,
+//     },
+//     end:{
+//       x:120,
+//       rotate:360,
+//     }
+//   }
+//   return (
+    
+//     <motion.div variants={move} animate="end" initial="start"
+//     transition={
+//       {
+//         duration:2,
+//         delay:1,
+//       }
+//     }
+    
+//     id='box'></ motion.div>
+    
+
+// )}
+
+// export default App
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
   )
 }
 
